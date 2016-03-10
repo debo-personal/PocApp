c82bdb1bc80e66b8ab6dad16927a0f0643923602
Debajit Majumder <debajit.business@gmail.com>
2016-03-10 09:17:48 +0530

    First commit to ember POC app

--- app/router.js
+++ app/router.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+const Router = Ember.Router.extend({ // cont is used for read only variable, so that it is not reassigned accidentally. 
   location: config.locationType
 });
 
 Router.map(function() {
+  this.route('scientists');
+  this.route('programmers');
+  this.route('about');
+  this.route('contact');
 });
 
 export default Router;
